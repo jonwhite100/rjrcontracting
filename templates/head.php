7029c007c76e07be7f68945a5f452450fc64fd2e
jonwhite100 <jon@bpm-web-design.co.uk>
2017-04-05 12:45:38 +0100

    updated loaction of font files

--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
 
-  <link rel="stylesheet" type="text/css" href="http://localhost/rjrcontracting/public_html/wp-content/themes/bpm-sage/assets/styles/handel.css">
+  <link rel="stylesheet" type="text/css" href="wp-content/themes/bpm-sage/assets/styles/handel.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
